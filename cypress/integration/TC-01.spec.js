fbd04d6335cfd1604d41ea40719b297bc5a4afec
krolcm <ana.mendes@run2biz.com>
2023-11-18 18:36:22 -0300

    alteração de script no package json

--- cypress/integration/TC-01.spec.js
+++ cypress/integration/TC-01.spec.js
@@ -2,5 +2,7 @@ describe('Acessar o Google', () => {
     it('Deve abrir a página do Google', () => {
       // Visitar a página do Google
       cy.visit('https://google.com')
+      cy.get('#APjFqb').type('Devops')
+      cy.get('#APjFqb').type('{enter}')
     })
   })
