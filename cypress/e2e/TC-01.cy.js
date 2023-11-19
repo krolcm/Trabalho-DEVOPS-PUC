778c0c78169fc8def8e97f085406c162aa23d482
krolcm <krolcm22@gmail.com>
2023-11-19 20:44:26 -0300

    FEAT: Nova funcionalidade de wait para aguardar carregando da pagina

--- cypress/e2e/TC-01.cy.js
+++ cypress/e2e/TC-01.cy.js
@@ -2,8 +2,9 @@ describe('Acessar a Amazon', () => {
     it('Deve abrir a página da Amazon', () => {
       // Visitar a página do Google
       cy.visit('https://amazon.com.br/');
+      cy.wait('3000');
       cy.get('#twotabsearchtextbox').type('Devops');
-      cy.get('#nav-search-submit-button').click();
-      
+      cy.get('#nav-search-submit-button').click(); 
+
     })
   })
