dcdb37d5673825e8cf7137b59ed9f90e2868fb1a
Igor <igorrc23@live.com>
2023-11-19 20:41:51 -0300

    escolhendo produto após pesquisa

---
+++ cypress/e2e/TC-02.cy.js
@@ -0,0 +1,19 @@
+describe('Acessar a Amazon', () => {
+    it('Deve abrir a página da Amazon, pesquisar por "Devops" e acessar o link "Manual de DevOps"', () => {
+      // Visitar a página da Amazon
+      cy.visit('https://amazon.com.br/');
+  
+      // Digitar "Devops" na caixa de pesquisa
+      cy.get('#twotabsearchtextbox').type('Devops');
+  
+      // Clicar no botão de pesquisa
+      cy.get('#nav-search-submit-button').click();
+  
+      // Aguardar resultados da pesquisa
+      cy.wait(2000); // Tempo de espera (ajuste conforme necessário)
+  
+      // Clicar no link "Manual de DevOps"
+      cy.contains('Manual de DevOps') // Seletor para encontrar o texto no link
+        .click(); // Clica no link
+    })
+  })
