084f0956d5d8f6a5b6882411971231a519125569
krolcm <krolcm22@gmail.com>
2023-11-19 21:06:58 -0300

    FIX: Corrigindo problemas de dependencias

--- cypress/e2e/TC-01.cy.js
+++ cypress/e2e/TC-01.cy.js
@@ -2,7 +2,7 @@ describe('Acessar a Amazon', () => {
     it('Deve abrir a página da Amazon', () => {
       // Visitar a página do Google
       cy.visit('https://amazon.com.br/');
-      cy.wait('3000');
+      cy.wait(3000);
       cy.get('#twotabsearchtextbox').type('Devops');
       cy.get('#nav-search-submit-button').click(); 
 
