590f8838e36bc787c7c7ad0b004d08e63015120e
krolcm <krolcm22@gmail.com>
2023-11-22 09:43:45 -0300

    FIX: Ajustando o TC-01

--- cypress/e2e/TC-01.cy.js
+++ cypress/e2e/TC-01.cy.js
@@ -1,8 +1,17 @@
 describe('Acessar a Amazon', () => {
-    it('Deve abrir a página da Amazon', () => {
-      // Visitar a página do Google
+    it('Deve abrir a página da Amazon, pesquisar por "Devops" e acessar o link "Manual de DevOps"', () => {
+      // Visitar a página da Amazon
       cy.visit('https://amazon.com.br/');
       cy.wait(5000);
-
+      // Digitar "Devops" na caixa de pesquisa
+      cy.get('#twotabsearchtextbox').type('Devops');
+      // Clicar no botão de pesquisa
+      cy.get('#nav-search-submit-button').click();
+      // Aguardar resultados da pesquisa
+      cy.wait(2000); // Tempo de espera (ajuste conforme necessário)
+      // Clicar no link "Manual de DevOps"
+      cy.contains('Manual de DevOps').click(); 
+      // Adicionar livro ao carrinho 
+      cy.get('#add-to-cart-button').click();
     })
   })
