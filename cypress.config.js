7221a790bd2fc6744c0f02415bcdbf6b9d5a5d20
krolcm <krolcm22@gmail.com>
2023-11-22 10:06:19 -0300

    FX: Ajustando record

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,8 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-    "projectId": "v17697"
+    }, 
+    "projectId": "v17697",
+    "record": true
   },
 });
